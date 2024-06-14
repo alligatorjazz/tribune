65565568e2be59c769b279d6ad699acdddb483f1
Damian Thomas <dimitrisonic@gmail.com>
2024-06-14 19:32:30 -0400

    custom intellisense!

--- src/renderer/src/routes/Pages/Edit.tsx
+++ src/renderer/src/routes/Pages/Edit.tsx
@@ -1,5 +1,6 @@
-import Editor from "@monaco-editor/react";
-import { useEffect, useMemo, useState } from "react";
+import Editor, { Monaco } from "@monaco-editor/react";
+import { IRange, editor, languages } from "monaco-editor";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { IndexSiteNode, NamedSiteNode } from "tribune-types";
 import { useAppContext } from "../../App.lib";
@@ -9,12 +10,50 @@ import { flattenSiteMap } from "../../lib";
 
 export function Edit() {
 	const [searchParams] = useSearchParams();
-	const [editorContent, setEditorContent] = useState<string | null>(null);
-	const [editorCache, setEditorCache] = useState<string | null>(null);
-
-	const [saving, setSaving] = useState(false);
 	const route = searchParams.get("route");
 	const { siteMap, setPreviewRoute } = useAppContext();
+
+	const [editorContent, setEditorContent] = useState<string | null>(null);
+	const [editorCache, setEditorCache] = useState<string | null>(null);
+	const initializeEditor = useCallback((editor: editor.IStandaloneCodeEditor, monaco: Monaco) => {
+		monaco.languages.html.htmlDefaults.setOptions({ suggest: { html5: true } });
+		monaco.languages.registerCompletionItemProvider("html", {
+			provideCompletionItems: (model, position) => {
+				const word = model.getWordUntilPosition(position);
+				const range: IRange = {
+					startLineNumber: position.lineNumber,
+					startColumn: word.startColumn,
+					endLineNumber: position.lineNumber,
+					endColumn: word.endColumn
+				};
+
+				// Define custom suggestions
+				const suggestions: languages.CompletionItem[] = [
+					{
+						label: "my-custom-tag", // The text that will be displayed in the suggestion list
+						kind: monaco.languages.CompletionItemKind.Snippet, // The kind/type of completion item
+						insertText: "<my-custom-tag></my-custom-tag>", // The text to insert when this suggestion is selected
+						insertTextRules:
+							monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, // Specifies that the insert text is a snippet
+						documentation: "My custom HTML tag", // Documentation for the suggestion (displayed as a tooltip),
+						range
+					},
+					{
+						label: "another-tag", // The text that will be displayed in the suggestion list
+						kind: monaco.languages.CompletionItemKind.Snippet, // The kind/type of completion item
+						insertText: "<another-tag></another-tag>", // The text to insert when this suggestion is selected
+						insertTextRules:
+							monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, // Specifies that the insert text is a snippet
+						documentation: "Another custom HTML tag", // Documentation for the suggestion (displayed as a tooltip)
+						range
+					}
+				];
+
+				// Return the suggestions
+				return { suggestions };
+			}
+		});
+	}, []);
 
 	const node = useMemo((): IndexSiteNode | NamedSiteNode | null => {
 		if (!Array.isArray(siteMap)) {
@@ -77,19 +116,16 @@ export function Edit() {
 			className="w-1/2"
 		>
 			{!node && <LoadingIndicator />}
-			{saving && (
-				<div className="flex gap-2">
-					<LoadingIndicator />
-					<p>Saving...</p>
-				</div>
-			)}
 			{node && editorContent && (
 				<Editor
 					language={"html"}
 					defaultValue={editorContent}
 					onChange={(value) => setEditorContent((prev) => value ?? prev)}
 					height={"80vh"}
+					/// <reference path="" />
+
 					options={{ wordWrap: "on" }}
+					onMount={initializeEditor}
 				/>
 			)}
 		</SidebarLayout>
