3315d030fee83c269476d0e10ca5978e6c7b27d0
Damian Thomas <dimitrisonic@gmail.com>
2024-07-13 17:55:22 -0400

    basic plug-and-play working

---
+++ Makefile
@@ -0,0 +1,3 @@
+build:
+	cargo build --target aarch64-apple-darwin
+	cargo build --target x86_64-pc-windows-gnu
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
 use std::{fs, io};
 
-fn main() ->io::Result<()>{
-	let paths = fs::read_dir(".")?
-		.map(|res| res.map(|file| file.path()));
+fn print_dir() {
+    // printing files in current working directory
+    let paths = fs::read_dir(".").unwrap().map(|res| res.map(|file| file.path()));
 
-	for path in paths {
-		let filename = path.unwrap();
-		let output = filename.to_str().unwrap();
-		println!("{output}");
-	}
+    for path in paths {
+        let filename = path.expect("Could not get path name.");
+        let output = filename.to_str().expect("Could not convert path to string.");
+        println!("{output}");
+    }
+}
 
-	Ok(())
+fn create_program_files() -> io::Result<()> {
+	// create build folder
+	fs::create_dir_all(".tribune")
+	// TODO: create config
+}
+
+// fn read_and_copy() {
+// 	// checks for a file called index.html and then appends a string to it
+// 	let signature = "<sig>Tribune was here!</sig>";
+// 	match fs::read("index.html") {
+// 		Ok(raw_buffer) => {
+// 			let content = String::from_utf8(raw_buffer)
+// 				.expect("Could not convert the file contents to a string.");
+// 			println!("Detected index.html file. Contents:\n{content}");
+// 			fs::write(".tribune/dist/index.html", format!("{content}\n\n{signature}")).expect("Could not write modified index.html.");
+
+// 		},
+// 		Err(err) => panic!("{err}")
+// 	}
+// }
+
+fn main() {
+	print_dir();
+	create_program_files().unwrap();
+
 }
