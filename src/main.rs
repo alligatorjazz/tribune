9cc38d026dec006dd59f07a1e74df45b94bca49a
Damian Thomas <dimitrisonic@gmail.com>
2024-07-13 18:47:49 -0400

    hot reloading / rebuilding

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::{fs, io};
+use std::{fs, io, path::Path};
+use notify::{FsEventWatcher, RecursiveMode, Watcher};
+use pathdiff::diff_paths;
+
+const WATCH_IGNOREDIRS: &[&str] = &[".tribune", ".git"];
+
 fn create_program_files() -> io::Result<()> {
 	// create build folder
 	fs::create_dir_all(".tribune")
@@ -19,8 +24,40 @@ fn build_site() -> io::Result<()>{
 	Ok(())
 }
 
-fn main() -> io::Result<()>{
+fn build_watcher() -> Result<FsEventWatcher, notify::Error> {
+
+	// Automatically select the best implementation for your platform.
+    notify::recommended_watcher(|res: Result<notify::Event, notify::Error>| {
+        match res {
+           Ok(event) => {
+				for path in event.paths {
+					let relative_path = diff_paths(path, Path::new(".")).unwrap();
+					let path_name = relative_path.to_str().unwrap();
+					
+					for ignored in WATCH_IGNOREDIRS {
+						let ignore_path = fs::canonicalize(ignored).unwrap();
+						let ignore_path_name = ignore_path.to_str().unwrap();
+						println!("Checking {path_name} against {ignore_path_name}");
+						if relative_path.starts_with(ignore_path) {
+							println!("Event in ignored path {ignored}, ignoring");
+							break;
+						} else {
+							build_site().unwrap();
+						}
+					}
+				}
+		   },
+           Err(e) => println!("watch error: {:?}", e),
+        }
+    })
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>>{
 	build_site()?;
+	println!("Starting watcher...");
+	let mut watcher = build_watcher()?;
+	watcher.watch(Path::new("."), RecursiveMode::Recursive)?;
+	println!("Starting server...");
 	devserver_lib::run("localhost", 8080, ".tribune", true, "");
 	Ok(())
 }
